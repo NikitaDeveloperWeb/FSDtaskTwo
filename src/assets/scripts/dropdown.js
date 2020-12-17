b64d2a0938656429d7a4fc6fd0b32b5cbc31620e
NikitaDe <69841196+NikitaDe@users.noreply.github.com>
2020-12-17 21:53:42 +0300

    set up the dropdown render

--- src/assets/scripts/dropdown.js
+++ src/assets/scripts/dropdown.js
@@ -1,9 +1,57 @@
 export class Dropdown {
   constructor(selector, options) {
     this.$el = document.getElementById(selector);
+    //input
     this.$arrow = this.$el.querySelector('.arrow__down');
+    //counter_block
+    this.$minus = this.$el.querySelector('.minus');
+    this.$plus = this.$el.querySelector('.plus');
+    this.$counter = this.$el.querySelector('.counter');
+    this.$listCategories = this.$el.querySelector('.list_categories');
+    //categories
+    this.categories = options.categories;
+    this.counter = 0;
 
+    //private methods
     this.#setup();
+    this.#render();
+  }
+
+  #render() {
+    let quantity = 0;
+    this.categories.forEach((element) => {
+      quantity = quantity + element.quantity;
+    });
+    this.counter = quantity;
+    //counter
+    if (this.counter === 0) {
+      document.getElementById('button_clear').classList.add('button_none');
+    } else {
+      document.getElementById('button_clear').classList.remove('button_none');
+    }
+
+    //render categories
+    let list = this.categories.map((obj) => {
+      return `
+      <li>
+      ${obj.name}
+      <div class="counter__block">
+        <div class="counter_method">
+          <svg width="6" height="2" viewBox="0 0 6 2" fill="none" xmlns="http://www.w3.org/2000/svg" class="minus">
+            <path d="M0.643477 0.564H5.35948V1.68H0.643477V0.564Z" fill="#1F2041" fill-opacity="0.5"></path>
+          </svg>
+        </div>  
+          <div class="counter">${obj.quantity}</div>
+        <div class="counter_method">  
+          <svg width="8" height="9" viewBox="0 0 8 9" fill="none" xmlns="http://www.w3.org/2000/svg" class="plus">
+            <path d="M7.91324 4.79762H4.56524V8.07362H3.43124V4.79762H0.0832422V3.73562H3.43124V0.441617H4.56524V3.73562H7.91324V4.79762Z" fill="#1F2041" fill-opacity="0.5"></path>
+          </svg>
+        </div>
+      </div>
+      </li>`;
+    });
+
+    this.$listCategories.innerHTML = [list];
   }
 
   #setup() {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import { Dropdown } from './assets/scripts/dropdown';
 // SCSS
 import './scss/main.scss';
 
-const dropdown = new Dropdown('guest', {});
+/*TODO:
+-Перенести блок счетчиков в js
+-Доделать счетчики
+*/
+
+const dropdown = new Dropdown('guest', {
+  categories: [
+    { name: 'ВЗРОСЛЫЕ', quantity: 0 },
+    { name: 'ДЕТИ', quantity: 0 },
+    { name: 'МЛАДЕНЦЫ', quantity: 0 },
+  ],
+});
 
 window.s = dropdown;
